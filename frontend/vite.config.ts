314b3ffb52d95ea0eb3c07dd7e09a73fea3db9e1
Rohit Datta <167865176+RohitDatta06@users.noreply.github.com>
2025-10-07 16:41:38 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-plugins: [react()],
-})
+  plugins: [react()],
+  server: {
+    host: true,              // allows local dev on all interfaces
+    port: 5173,
+  },
+  preview: {
+    port: 10001,             // or any
+    allowedHosts: [
+      "bm25-frontend.onrender.com"  // 👈 your Render frontend URL
+    ],
+  },
+});
